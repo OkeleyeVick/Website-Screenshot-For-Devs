ee9c25b20762f504a90007da29fc52988e8ecc02
Vickkk <justusofnigeria@gmail.com>
2023-03-15 15:05:08 +0100

    test 1 passed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				main: "#731DD8",
+			},
+		},
 	},
 	plugins: [],
 };

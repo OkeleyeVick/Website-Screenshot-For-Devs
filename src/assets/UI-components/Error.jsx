b7caaba74192b7817f122cfc7bc01bcf6d2a572a
Vickkk <justusofnigeria@gmail.com>
2023-03-19 12:49:42 +0100

    updates

--- src/assets/UI-components/Error.jsx
+++ src/assets/UI-components/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
 	const { error } = useContext(sizeContext);
 
 	return (
-		<div className="fixed top-1 right-1 md:top-2 md:right-2 py-3 px-3 shadow-custom_4 rounded-md flex items-center gap-x-4 bg-white z-40 w-52 xs:w-72 md:w-80">
+		<div className="fixed top-2 right-2 md:top-2 md:right-2 py-3 px-3 shadow-custom_4 rounded-md flex items-center gap-x-4 bg-white z-40 w-52 xs:w-72 md:w-80">
 			<Icon icon="icon-park-solid:caution" className="w-4 h-4 text-[#DD2E44]" />
 			<span className="text-xs font-semibold leading-4 flex-1">{error}</span>
 		</div>
